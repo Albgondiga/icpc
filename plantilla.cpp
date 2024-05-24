361140db731f76236d1bf96343faf6110ba19806
Alberto González Avendaño <albertoga03@gmail.com>
2024-05-23 21:12:13 -0600

    Actualizacion de plantilla

--- plantilla.cpp
+++ plantilla.cpp
@@ -6,11 +6,14 @@ using namespace std;
 #define zero(v) memset(v, 0, sizeof(v))
 #define forall(it, v) for (auto it=v.begin(); it!=v.end(); ++it)
 #define pb push_back
+#define pf push_front
+#define mp make_pair
 #define fst first
 #define snd second
 typedef long long ll;
 typedef pair<int, int> ii;
 typedef pair<string, int> si;
+typedef pair<ll,ll> pll;
 #define dforn(i, n) for (int i=n-1; i>=0; i--)
 #define dprint(v) cout<<#v"="<<v<<endl
 const int MAXN=100100;
