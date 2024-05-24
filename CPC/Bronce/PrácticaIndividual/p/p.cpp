a67fbd0949748d3907f8b9b3933ce617b5a2ea94
Alberto González Avendaño <albertoga03@gmail.com>
2024-05-24 17:48:25 -0600

    Solucion de p

--- CPC/Bronce/PrácticaIndividual/p/p.cpp
+++ CPC/Bronce/PrácticaIndividual/p/p.cpp
@@ -6,43 +6,42 @@ using namespace std;
 #define zero(v) memset(v, 0, sizeof(v))
 #define forall(it, v) for (auto it=v.begin(); it!=v.end(); ++it)
 #define pb push_back
+#define pf push_front
+#define mp make_pair
 #define fst first
 #define snd second
 typedef long long ll;
 typedef pair<int, int> ii;
 typedef pair<string, int> si;
+typedef pair<ll,ll> pll;
 #define dforn(i, n) for (int i=n-1; i>=0; i--)
 #define dprint(v) cout<<#v"="<<v<<endl
 const int MAXN=100100;
 
-#define debug 0
+#define debug 1
 #define ifd if (debug)
-
-bool compareRate(pair<long double,int>& p1, pair<long double,int>& p2) { 
-    if (p1.first != p2.first) {
-        return p1.first > p2.first;
-    } else {
-        return p1.second < p2.second;
-    }
-} 
 
 int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(false);
 
-    ll N; cin>>N;
-    vector<pair<long double,int>> rate(N);
-    forn(i,N) {
-        long double A, B; cin>>A>>B;
-        long double r = A/(A+B);
-        ifd cout<<"r = "<<r<<endl;
-        rate[i] = make_pair(r,i+1);
+    string inst;
+    priority_queue<int> S;
+    vector<int> n;
+    while (cin >> inst) {
+        if (inst == "insert") {
+            int x;
+            cin >> x;
+            S.push(x);
+        } else if (inst == "extract") {
+            n.push_back(S.top());
+            S.pop();
+        } else {
+            for (auto i : n) {
+                cout << i << endl;
+            }
+            return 0;
+        }
     }
-    sort(rate.begin(), rate.end(), compareRate);
-    forn(i,N) {
-        ifd cout<<rate[i].first<<" "<<rate[i].second<<"\n";
-        cout<<rate[i].second<<" ";
-    }
-    cout<<"\n";
     return 0;
 }
