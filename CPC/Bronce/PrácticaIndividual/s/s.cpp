bfe19d2d25e65b4cfaabb5608ae8971ecf09e542
Alberto González Avendaño <albertoga03@gmail.com>
2024-05-24 18:14:56 -0600

    Solucion de s

---
+++ CPC/Bronce/PrácticaIndividual/s/s.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std; 
+#define forr(i, a, b) for (int i=a; i<b; i++)
+#define forn(i, n) forr(i, 0, n)
+#define sz(c) ((int)c.size())
+#define zero(v) memset(v, 0, sizeof(v))
+#define forall(it, v) for (auto it=v.begin(); it!=v.end(); ++it)
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define fst first
+#define snd second
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<string, int> si;
+typedef pair<ll,ll> pll;
+#define dforn(i, n) for (int i=n-1; i>=0; i--)
+#define dprint(v) cout<<#v"="<<v<<endl
+const int MAXN=100100;
+
+#define debug 1
+#define ifd if (debug)
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    ll Q; cin>>Q;
+    map<ll, ll> m;
+
+    forn(k,Q) {
+        ll i; cin>>i;
+        if (i == 0) {
+            ll k, v; cin>>k>>v;
+            m[k] = v;
+        } else {
+            ll k; cin>>k;
+            cout<<m[k]<<"\n";
+        }
+    }
+}
