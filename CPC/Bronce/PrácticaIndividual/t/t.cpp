edfb07950bbb10a48707ff772b36b9f39d9b5d87
Alberto González Avendaño <albertoga03@gmail.com>
2024-05-24 18:32:18 -0600

    Solucion de t

---
+++ CPC/Bronce/PrácticaIndividual/t/t.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std; 
+#define forr(i, a, b) for (int i=a; i<b; i++)
+#define forn(i, n) forr(i, 0, n)
+#define sz(c) ((int)c.size())
+#define zero(v) memset(v, 0, sizeof(v))
+#define forall(it, v) for (auto it=v.begin(); it!=v.end(); ++it)
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define fst first
+#define snd second
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<string, int> si;
+typedef pair<ll,ll> pll;
+#define dforn(i, n) for (int i=n-1; i>=0; i--)
+#define dprint(v) cout<<#v"="<<v<<endl
+const int MAXN=100100;
+
+#define debug 1
+#define ifd if (debug)
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    ll t; cin>>t;
+    forn(i,t) {
+        ll n; cin>>n;
+        vector<ll> a(n);
+        priority_queue<ll, vector<ll>, greater<ll>> q;
+        forn(j,n) {
+            ll x; cin>>x;
+            q.push(x);
+        }
+        ll count = 0;
+        while (q.size() >= 2) {
+            ll x = q.top();
+            q.pop();
+            ll y = q.top();
+            q.pop();
+            count += (x+y);
+            q.push(x+y);
+        }
+        cout<<count<<"\n";
+    }
+}
