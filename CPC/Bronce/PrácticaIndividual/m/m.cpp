33b3e651439cb1612b9baede45cafba37a0a68bb
Alberto González Avendaño <albertoga03@gmail.com>
2024-05-24 21:58:22 -0600

    Otro intento fallido de m

--- CPC/Bronce/PrácticaIndividual/m/m.cpp
+++ CPC/Bronce/PrácticaIndividual/m/m.cpp
@@ -21,9 +21,6 @@ const int MAXN=100100;
 #define debug 0
 #define ifd if (debug)
 
-#define right 1
-#define left -1
-
 int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(false);
@@ -33,27 +30,23 @@ int main() {
     forn(i,n) cin>>a[i];
     sort(a.begin(),a.end());
 
-    ll i = n/2;
-    ll dir = right;
-    forn(j,k) {
-        ifd cout<<"i="<<i<<endl;
-        if (dir == right && i == a.size()-1) {
-            a[i]++;
-            i = n/2;
-        } else if (dir == left && i == n/2) {
-            a[i]++;
-            dir = right;
-        } else if (dir == right) {
-            a[i]++;
-            if (a[i] > a[i+1])
-                i++;
-        } else if (dir == left) {
-            a[i]++;
-            if (a[i] > a[i-1])
-                i--;
+    ll original = a[n/2];
+    ll maxMedian = original+k;
+    while (maxMedian > original) {
+        ll op = 0;
+        bool isMax = true;
+        for(ll i = n/2; i < n && isMax; i++) {
+            if (maxMedian-a[i] > 0) {
+                op += maxMedian-a[i];
+                if (op > k)
+                    isMax = false;
+            }
         }
+        ifd cout<<"m = "<<maxMedian<<", ";
+        ifd cout<<"op = "<<op<<endl;
+        if (isMax)
+            break;
+        maxMedian--;
     }
-    sort(a.begin(),a.end());
-    ifd cout<<"i="<<i<<endl;
-    cout<<a[n/2]<<"\n";
+    cout<<maxMedian<<"\n";
 }
