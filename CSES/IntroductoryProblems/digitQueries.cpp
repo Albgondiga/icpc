940ff3818206a64d7012f0656c52f4327ce1bc54
Alberto González Avendaño <albertoga03@gmail.com>
2024-05-28 21:31:38 -0600

    Intento de Digit Queries

--- CSES/IntroductoryProblems/digitQueries.cpp
+++ CSES/IntroductoryProblems/digitQueries.cpp
@@ -13,18 +13,49 @@ typedef pair<int, int> ii;
 typedef pair<string, int> si;
 #define dforn(i, n) for (int i=n-1; i>=0; i--)
 #define dprint(v) cout<<#v"="<<v<<endl
-const int MAXN=100100;
+const int MAXn=100100;
 
-#define debug 1
+#define MOD 1000000
+
+#define debug 0
 #define ifd if (debug)
+
+ll binpow(ll a, ll b, ll m) {
+    if (b == 0)
+        return 1;
+
+    if (b % 2)
+        return binpow(a, b-1, m) * a % m;
+
+    ll res = binpow(a, b/2, m);
+    return res*res % m;
+}
+
+ll getDigit(ll k){
+    if (k <= 9)
+        return k;
+
+    ll len = 1;
+    while(9 * binpow(len-1, 10, MOD) * len < k-1){
+        k -= 9 * binpow(len-1, 10, MOD) * len;
+        len++;
+    }
+
+    ll value = binpow(len-1, 10, MOD) + k-1/len - 1;
+    ifd cout<<"len == "<<len<<endl;
+    ifd cout<<"value == "<<value<<endl;
+    return value / (binpow(10, (value % len)+1, MOD));
+}
 
 int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(false);
 
-    int q; cin>>q;
-    forn(i,q) {
+    ll q; cin>>q;
+    for (ll i = 0; i < q; i++) {
         ll k; cin>>k;
-        
+        cout<<getDigit(k)<<"\n";
     }
+
+    return 0;
 }
