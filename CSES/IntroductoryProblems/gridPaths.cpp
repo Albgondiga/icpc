6eb75c09203d2cd0dd2b3311eefa5b7f819b5587
Alberto González Avendaño <albertoga03@gmail.com>
2024-06-02 01:02:20 -0600

    Intento de Grid Paths (TLE)

--- CSES/IntroductoryProblems/gridPaths.cpp
+++ CSES/IntroductoryProblems/gridPaths.cpp
@@ -18,89 +18,122 @@ const int MAXN=100100;
 #define debug 1
 #define ifd if (debug)
 
-ii nextPos(char dir, ii pos) {
-    if (dir == 'U') {
-        return make_pair(pos.first-1,pos.second);
-    } else if (dir == 'D') {
-        return make_pair(pos.first+1,pos.second);
-    } else if (dir == 'L') {
-        return make_pair(pos.first,pos.second-1);
-    } else if (dir == 'R') {
-        return make_pair(pos.first,pos.second+1);
+vector<vector<bool>> grid(7, vector<bool>(7,false));
+string path;
+
+// Dir para der, izq, arr, ab
+int dx[4] = { 0, 0, -1, 1 };
+int dy[4] = { 1, -1, 0, 0 };
+
+#define right 0
+#define left 1
+#define up 2
+#define down 3
+
+bool isValid(ll x, ll y) {
+    return x >= 0 && x < 7 && y >= 0 && y < 7;
+}
+
+bool end(ll x, ll y) {
+    return x == 6 && y == 0;
+}
+
+int getDir(char d) {
+    if (d == 'R') {
+        return right;
+    } else if (d == 'L') {
+        return left;
+    } else if (d == 'U') {
+        return up;
+    } else if (d == 'D') {
+        return down;
     }
 }
 
-bool isValid(ii pos) {
-    return pos.first >= 1 && pos.first <= 7
-        && pos.second >= 1 && pos.second <= 7;
-}
-
-bool end(ii pos) {
-    return pos.first == 7 & pos.second == 1;
-}
-
-void countPaths(string& path, ii pos, int index, ll& count, vector<vector<bool>>& grid) {
+void countPaths(ll x, ll y, int index, ll& count) {
     // Si se llega al final de la cadena  y a la posicion correcta
     if (index == 48) {
-        if (end(pos)) {
+        if (end(x,y)) {
             count++;
-            ifd cout<<"count++\n";
-            forn(i,7) {
-                forn(j,7) {
-                    cout<<grid[i][j]<<" ";
-                }
-                cout<<endl;
-            }
         }
         return;
     }
     // Si se llega al final de forma prematura retorna
-    if (end(pos)) 
+    if (end(x,y)) 
         return;
     // Si ya paso por ahi, retorna
-    if (grid[pos.first-1][pos.second-1])
+    if (grid[x][y])
         return;
+
+    // Celdas vecinas
+    vector<bool> visited(4, -1);
+    for (int i = 0; i < 4; i++)
+        if (isValid(x+dx[i],y+dy[i]))
+            visited[i] = grid[x+dx[i]][y+dy[i]];
+
+    // Particiones:
+    // No he ido arriba ni abajo pero si izq y der
+    if (!visited[down] && !visited[up] && visited[right]
+        && visited[left])
+        return;
+    // No he ido izq y der pero sí arriba y abajo
+    if (!visited[right] && !visited[left] && visited[down]
+        && visited[up])
+        return;
+
+    // ME FALTARON ESTOS CASOS (por eso me daba TLE):
+
+    // If we are at a position such that the upper-right
+    // diagonal square is visited and the up and right
+    // squares are unvisited return 0
+    if (isValid(x-1,y+1) && grid[x-1][y+1])
+        if (!visited[right] && !visited[up])
+            return;
+
+    // If we are at a position such that the lower-right
+    // diagonal square is visited and the down and right
+    // squares are unvisited return 0
+    if (isValid(x+1,y+1) && grid[x+1][y+1])
+        if (!visited[right] && !visited[down])
+            return;
+
+    // If we are at a position such that the upper-left
+    // diagonal square is visited and the up and left
+    // squares are unvisited return 0
+    if (isValid(x-1,y-1) && grid[x-1][y-1])
+        if (!visited[left] && !visited[up])
+            return;
+
+    // If we are at a position such that the lower-left diagonal
+    // square is visited and the down and right squares are
+    // unvisited return 0
+    if (isValid(x+1,y-1) && grid[x+1][y-1])
+        if (!visited[left] && !visited[down])
+            return;
+
     // Si no, continua
-    grid[pos.first-1][pos.second-1] = true;
+    grid[x][y] = true;
     if (path[index] == '?') {
-        ii newPos = nextPos('U', pos);
-        if (isValid(newPos) && grid[newPos.first-1][newPos.second-1] == false)
-            countPaths(path, newPos, index+1, count, grid);
-        
-        newPos = nextPos('D', pos);
-        if (isValid(newPos) && grid[newPos.first-1][newPos.second-1] == false)
-            countPaths(path, newPos, index+1, count, grid);
-
-        newPos = nextPos('L', pos);
-        if (isValid(newPos) && grid[newPos.first-1][newPos.second-1] == false)
-            countPaths(path, newPos, index+1, count, grid);
-
-        newPos = nextPos('R', pos);
-        if (isValid(newPos) && grid[newPos.first-1][newPos.second-1] == false)
-            countPaths(path, newPos, index+1, count, grid);
+        forn(i,4) {
+            if (isValid(x+dx[i],y+dy[i])) {
+                countPaths(x+dx[i],y+dy[i],index+1,count);
+            }
+        }
     } else {
-        ii newPos = nextPos(path[index], pos);
-        // ifd cout<<path[index]<<" => "<<"["<<newPos.first<<"]"<<"["<<newPos.second<<"]"<<"\n";
-        if (isValid(newPos) && grid[newPos.first-1][newPos.second-1] == false)
-            countPaths(path, newPos, index+1, count, grid);
+        int dir = getDir(path[index]);
+        if (isValid(x+dx[dir],y+dy[dir])) {
+            countPaths(x+dx[dir], y+dy[dir], index+1, count);
+        }
     }
-    grid[pos.first-1][pos.second-1] = false;
+    grid[x][y] = false;
 }
 
 int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(false);
 
-    vector<vector<bool>> grid(7, vector<bool>(7,false));
-    forn(i,7) {
-        forn(j,7) {
-            cout<<grid[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-
-    string path; cin>>path;
+    cin>>path;
     ll count = 0;
-    countPaths(path, make_pair(1,1), 0, count, grid);
+    countPaths(0, 0, 0, count);
     cout<<count<<"\n";
 }
