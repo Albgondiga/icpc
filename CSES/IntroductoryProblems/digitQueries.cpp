57cef58e76a6417b17fc002c1d2cdff83c7e1dfe
Alberto González Avendaño <albertoga03@gmail.com>
2024-06-14 19:40:46 -0600

    digitQueries

--- CSES/IntroductoryProblems/digitQueries.cpp
+++ CSES/IntroductoryProblems/digitQueries.cpp
@@ -15,47 +15,45 @@ typedef pair<string, int> si;
 #define dprint(v) cout<<#v"="<<v<<endl
 const int MAXn=100100;
 
-#define MOD 1000000
+#define MOD 100000007
 
 #define debug 0
 #define ifd if (debug)
 
-ll binpow(ll a, ll b, ll m) {
-    if (b == 0)
-        return 1;
-
-    if (b % 2)
-        return binpow(a, b-1, m) * a % m;
-
-    ll res = binpow(a, b/2, m);
-    return res*res % m;
-}
-
-ll getDigit(ll k){
-    if (k <= 9)
-        return k;
-
-    ll len = 1;
-    while(9 * binpow(len-1, 10, MOD) * len < k-1){
-        k -= 9 * binpow(len-1, 10, MOD) * len;
-        len++;
-    }
-
-    ll value = binpow(len-1, 10, MOD) + k-1/len - 1;
-    ifd cout<<"len == "<<len<<endl;
-    ifd cout<<"value == "<<value<<endl;
-    return value / (binpow(10, (value % len)+1, MOD));
+ll binpow(ll a, ll b) {
+    if (b == 0) return 1;
+    if (b % 2) return binpow(a,b-1) * a;
+    ll res = binpow(a,b/2);
+    return res * res;
 }
 
 int main() {
-    cin.tie(0);
-    ios_base::sync_with_stdio(false);
+    // Example 1
+    ll q; cin >> q;
+    forn (i, q) {
+        ll k; cin >> k;
 
-    ll q; cin>>q;
-    for (ll i = 0; i < q; i++) {
-        ll k; cin>>k;
-        cout<<getDigit(k)<<"\n";
+        // No of digits
+        ll digits = 1;
+        // Total numbers in current digit interval
+        ll base = 9;
+
+        // Find the interval in which the Nth digit lies
+        while (k - digits * base > 0) {
+            k -= digits * base;
+            base *= 10;
+            digits++;
+        }
+        ll index = k % digits;
+
+        // Calculate the number which contains the kth digit
+        ll res = binpow(10, (digits - 1)) + (k - 1) / digits;
+
+        // Find out which digit in the number is the result
+        if (index != 0)
+            res = res / binpow(10, digits - index);
+
+        cout<<res%10<<"\n";
     }
-
     return 0;
 }
