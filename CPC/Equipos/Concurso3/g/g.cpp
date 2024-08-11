fcd63f428347059e7acc5e9acd44ed35f7f743a2
Alberto González Avendaño <albertoga03@gmail.com>
2024-08-10 18:47:42 -0600

    G de Concurso 3

--- CPC/Equipos/Concurso3/g/g.cpp
+++ CPC/Equipos/Concurso3/g/g.cpp
@@ -15,25 +15,122 @@ typedef pair<int, int> ii;
 #define dprint(v) cout<<#v"="<<v<<endl
 const int MAXN=100100;
 
-#define debug 1
+#define debug 0
+#define ifd if (debug)
+vector<int> link, size;
 
+int find(int x) {
+    while(x!=link[x]) x= link[x];
+    return x;
+}
+
+struct UF {
+    vector<int> link, size;
+    UF(int n) {
+        link.resize(n);
+        size.resize(n);
+        forr(i,1, n+1) link[i] = i;
+        forr(i, 1, n+1) size[i]=1;
+    }
+
+    int find(int x) {
+        while(x!=link[x]) x= link[x];
+        return x;
+    }
+
+    bool same(int a, int b ) {
+        return find(a)==find(b);
+    }
+
+    void unite(int a, int b) {
+        a=find(a);
+        b=find(b);
+        if (size[a]<size[b]) swap(a, b);
+        size[a]+=size[b];
+        link[b] = a;
+    }
+};
+
+vector<int> distA, distB;
+vector<vector<int> > adyacencia;
+vector<bool> seenA, seenB;
+void dfsA(int a) {
+    seenA[a]=true;
+    ifd cout<<"llamo dfsA sobre a = "<<a<<endl;
+    for (auto u:adyacencia[a]) {
+        if (seenA[u]) continue;
+        distA[u]=distA[a]+1;
+        dfsA(u); 
+    }
+}
+
+void dfsB(int b) {
+    seenB[b]=true;
+    for (auto u:adyacencia[b]) {
+        if (seenB[u]) continue;
+        distB[u]=distB[b]+1;
+        dfsB(u); 
+    }
+}
 
 int main() {
 
     cin.tie(0);
     ios_base::sync_with_stdio(false);
 
-    int n; cin>>n;
-
-    
-    int i1=1, i2=n, i3;
-    while(i1!=i2-1) {
-        i3=(i2-i1)/2 +i1;
-        cout<<"? "<<i3<<endl;
-        int res; cin>>res;
-        if (res==0) i1=i3;
-        else i2=i3;
+    int n, m;
+    cin>>n>>m;
+    UF uF(n);
+    vector<pair<ll, ii> > adj(m);
+    forn(i, m) {
+        int a, b; ll x;
+        cin>>a>>b>>x;
+        a--; b--;
+        adj[i] = {x, {a, b}};
+        // if (!uF.same(a, b)) {
+        //     ifd cout<<a<<" y "<<b<<" están en el mismo ahora"<<endl;
+        //     uF.unite(a, b);
+        // }
     }
-    cout<<"! "<<i1<<endl;
-    return 0;
-}   
+
+    sort(adj.begin(), adj.end());
+
+    ll cost=0;
+    stack<ii> aristas;
+    forn(i, m) {
+        int a = adj[i].second.first, b = adj[i].second.second;
+        if (!uF.same(a, b)) {
+            uF.unite(a, b);
+            aristas.push({a, b});
+            cost+=adj[i].first;
+        }
+    }
+    
+    cout<<cost<<endl;
+    adyacencia.resize(n);
+    while(!aristas.empty()) {
+        ii current=aristas.top();
+        aristas.pop();
+        adyacencia[current.first].push_back(current.second);
+        adyacencia[current.second].push_back(current.first);
+    }
+
+    seenA.resize(n); seenB.resize(n);
+    distA.resize(n); distB.resize(n);
+    distA[0]=0;
+    dfsA(0);
+    int b=0;
+    forn(i, n) if (distA[i]>distA[b]) b=i;
+    if (debug) cout<<"b = "<<b<<endl; 
+    distB[b]=0;
+    dfsB(b);
+    int c = 0;
+    forn(i, n) if (distB[i]>distB[c]) c=i;
+    
+
+    cout<<distB[c]<<endl;
+    // cout<<maxCosto<<endl;
+
+
+
+}
