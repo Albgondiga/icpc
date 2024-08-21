642900e53c807c76068925db9c64cd1625fb8053
Alberto González Avendaño <albertoga03@gmail.com>
2024-08-20 21:42:36 -0600

    List Removals, ejercicios de Range Queries de CSES

---
+++ CSES/RangeQueries/listRemovals.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std; 
+#define forr(i, a, b) for (int i=a; i<b; i++)
+#define forn(i, n) forr(i, 0, n)
+#define sz(c) ((int)c.size())
+#define zero(v) memset(v, 0, sizeof(v))
+#define forall(it, v) for (auto it=v.begin(); it!=v.end(); ++it)
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define fst first
+#define snd second
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<string, int> si;
+typedef pair<ll,ll> pll;
+#define dforn(i, n) for (int i=n-1; i>=0; i--)
+#define dprint(v) cout<<#v"="<<v<<endl
+
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+  
+#define ordered_set tree<pair<int,ll>, null_type,less<pair<int,ll>>, rb_tree_tag,tree_order_statistics_node_update> 
+
+#define debug 1
+#define ifd if (debug)
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int n; cin>>n;
+
+    ordered_set s;
+    forn(i,n) {
+        ll x; cin>>x;
+        s.insert({i,x}); 
+    }
+
+    forn(i,n) {
+        int p; cin>>p;
+        auto it = s.find_by_order(p-1);
+        s.erase(it);
+        cout<<(*it).second<<" ";
+    }
+    cout<<"\n";
+
+    return 0;
+}
