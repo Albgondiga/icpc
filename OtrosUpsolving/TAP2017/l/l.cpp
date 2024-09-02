08213a145dac7f5efeff56fe9774218f77348b5a
Alberto González Avendaño <albertoga03@gmail.com>
2024-09-02 11:01:33 -0600

    L de TAP2017

--- OtrosUpsolving/TAP2017/l/l.cpp
+++ OtrosUpsolving/TAP2017/l/l.cpp
@@ -1,4 +1,3 @@
-// 
 #include <bits/stdc++.h>
 using namespace std; 
 #define forr(i, a, b) for (int i=a; i<b; i++)
@@ -18,50 +17,55 @@ const int MAXN=100100;
 #define debug 1
 #define ifd if (debug)
 
-int main() {
+const ll INF = 1e18;
 
+ll tomar2(ll& fig1, ll& fig2) {
+    ll x = min(fig1/2, fig2/2);
+    fig1 -= 2LL*x;
+    fig2 -= 2LL*x;
+    return x;
+}
+
+ll tomar4(ll& fig) {
+    ll x = fig/4;
+    fig -= 4LL*x;
+    return x;
+}
+
+ll casoRaro(ll& fig1, ll& fig2, ll& fig3) {
+    ll x = min(fig1, min(fig2, fig3/2));
+    fig1 -= x;
+    fig2 -= x;
+    fig3 -= 2LL*x;
+    return x;
+}
+
+ll comb(ll l, ll o, ll s, ll t) {
+    if (l < 0 || o < 0 || s < 0 || t < 0) return -1LL*INF;
+    ll count = 0;
+    count += tomar2(l, s);
+    count += tomar4(l);
+    count += tomar4(o);
+    count += tomar4(t);
+    return count;
+}
+
+int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(false);
 
     ll l, o, s, t; cin>>l>>o>>s>>t;
-    ll count = 0;
-    if (l >= 2 && s >= 2) {
-        ll x = min(l/2, s/2);
-        count += x;
-        l -= 2*x;
-        s -= 2*x;
-    }
-    if (l >= 2 && o >= 2) {
-        ll x = min(l/2, o/2);
-        count += x;
-        l -= 2*x;
-        o -= 2*x;
-    }
-    if (l >= 4) {
-        ll x = l/4;
-        count += x;
-        l -= 4*x;
-    }
-    if (o >= 4) {
-        ll x = o/4;
-        count += x;
-        o -= 4*x;
-    }
-    if (t >= 4) {
-        ll x = t/4;
-        count += x;
-        t -= 4*x;
-    }
-    if (l >= 1 && s >= 1 && t >= 2) {
-        ll x = min(l, min(s, t/2));
-        count += x;
-        l -= x;
-        s -= x;
-        t -= 2*x;
-    }
+    ll count = 0; 
+    // LLOO: 0 LSTT: 0 
+    count = max(count, comb(l,o,s,t));
+    // LLOO: 1 LSTT: 0 
+    count = max(count, comb(l-2,o-2,s,t)+1);
+    // LLOO: 0 LSTT: 1 
+    count = max(count, comb(l-1,o,s-1,t-2)+1);
+    // LLOO: 1 LSTT: 1
+    count = max(count, comb(l-3,o-2,s-1,t-2)+2);
 
     cout<<count<<"\n";
 
     return 0;
-
 }
