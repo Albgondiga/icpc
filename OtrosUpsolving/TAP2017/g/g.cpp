ca290467a5a5266a6023a930607664e1f3b33522
Alberto González Avendaño <albertoga03@gmail.com>
2024-09-02 11:01:55 -0600

    Intento de G de TAP2017

--- OtrosUpsolving/TAP2017/g/g.cpp
+++ OtrosUpsolving/TAP2017/g/g.cpp
@@ -13,37 +13,17 @@ typedef long long ll;
 typedef pair<int, int> ii;
 #define dforn(i, n) for (int i=n-1; i>=0; i--)
 #define dprint(v) cout<<#v"="<<v<<endl
-const int MAXN=100100;
 
 #define debug 1
 #define ifd if (debug)
 
 const ll MOD = 1e9+7;
-const ll N = 102;
-vector<ll> factoriales(N);
-vector<ll> inv(N);
-bitset<1005> seen[1005];
-vector<vector<ll> > dp(1005, vector<ll> (1005));
-ll m;
-
-ll f(ll p, ll i) {
-    if (p==0) return 1;
-    if (p<0) return 0;
-    if (i==1) {
-        if (p<=m) return 1;
-        else return 0;
-    }
-
-    if (seen[p][i]) return dp[p][i];
-    seen[p][i] =1;
-    ll ans=0;
-    for (int j=0; j<=m; j++) {
-        ans+=f(p-j, i-1);
-        ans%=MOD;
-    }
-    return dp[p][i]=ans;
-}
-
+const ll MAXN = 105;
+const ll MAXP = 1005;
+vector<ll> factoriales(MAXN);
+vector<ll> inv(MAXN);
+ll dp[MAXN][MAXP];
+ll n, m, p1, p2;
 
 ll exp(ll b, ll e) {
     ll r = 1;
@@ -60,48 +40,53 @@ ll Binom(ll n, ll k) {
     return (factoriales[n] * inv[k] % MOD * inv[n-k] % MOD);
 }
 
+// Numero de maneras de hacer j puntos en i rondas, haciendo
+// entre 1 y M puntos en cada una
+ll f(ll i, ll j) {
+    if (j == 0) return 1;
+    if (j < 0) return 0; 
+    if (i == 1) {
+        if (j <= m) return 1;
+        else return 0;
+    }
+
+    if (dp[i][j] == -1) {
+        ll ans = 0;
+        for (ll p = 1; p <= m; p++) {
+            ans += f(i-1, j-p);
+            ans %= MOD;
+        }  
+        dp[i][j] = ans;
+    }
+    return dp[i][j];
+}
+
 int main() {
 
     cin.tie(0);
     ios_base::sync_with_stdio(false);
 
-    ll n, p1, p2;
     cin>>n>>m>>p1>>p2;
 
     factoriales[0] = 1;
-    for (ll i = 1; i < N; i++) {
+    for (ll i = 1; i < MAXN; i++) {
         factoriales[i] = factoriales[i-1] * i % MOD;
     }
 
-    inv[N-1] = exp(factoriales[N-1], MOD-2);
-    for (ll i = N-2; i >= 0; i--) {
+    inv[MAXN-1] = exp(factoriales[MAXN-1], MOD-2);
+    for (ll i = MAXN-2; i >= 0; i--) {
         inv[i] = inv[i+1] * (i+1) % MOD;
     }
-    
-    ll minP1=p1/m;
-    ll minP2=p2/m;
-    ll minP=min(minP1, minP2);
-    ll otroMinP=max(minP1, minP2);
 
-    ll ans=0;
-    for (int i=minP1; n-i>=otroMinP; i++) {
-        ll cA=Binom(n, i);
-        ifd cout<<"vea que "<<n<<"escojo "<<i<<"="<<cA<<endl;
-        cA%=MOD;
-        ll f1=f(p1, i);
-        ifd cout<<"vea que f("<<p1<<", "<<i<<") = "<<f1<<endl;
+    forn(i, MAXN) forn(j, MAXP) dp[i][j] = -1;
 
-        cA*=f1;
-        cA%=MOD;
-        ll f2=f(p2, n-i);
-        ifd cout<<"vea que f("<<p2<<", "<<n-i<<") = "<<f2<<endl;
-        cA*=f2;
-        cA%=MOD;
-        ans+=cA;
-        ans%=MOD;
+    ll ans = 0;
+    for (ll i = 0; i <= n; i++) {
+        ll j = n-i;
+        ans += f(i,p1) * f(j,p2) * Binom(n,i) * Binom(n-i,j);
+        ans %= MOD;
     }
 
-
-    cout<<ans<<endl;
+    cout<<ans<<"\n";
     return 0;
 }
