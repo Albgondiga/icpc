65868b3a731115b3a584ea38ffe33c527ad0008e
Alberto Gonzalez Avendano <albertoga03@gmail.com>
2024-11-04 15:42:28 -0600

    Contest 20/10/2024

---
+++ Codeforces/Contests/20oct2024/b/b.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std; 
+#define forr(i, a, b) for (int i=a; i<b; i++)
+#define forn(i, n) forr(i, 0, n)
+#define sz(c) ((int)c.size())
+#define zero(v) memset(v, 0, sizeof(v))
+#define forall(it, v) for (auto it=v.begin(); it!=v.end(); ++it)
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define fst first
+#define snd second
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<string, int> si;
+typedef pair<ll,ll> pll;
+#define dforn(i, n) for (int i=n-1; i>=0; i--)
+#define dprint(v) cout<<#v"="<<v<<endl
+
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+  
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+
+#define debug 0
+#define ifd if (debug)
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int t; cin>>t;
+    ll n, k;
+    while (t--) {
+        cin>>n>>k;
+        vector<ll> a(n);
+        forn(i,n) cin>>a[i];
+        sort(a.begin(), a.end());
+
+        if (n*a[0] >= k) cout<<k<<"\n";
+        else {
+            ll sum = n*a[0];
+            int innecesarios = -1;
+            for (int i = 1; i < n; i++) {
+                sum += (n-i)*(a[i]-a[i-1]);
+                if (sum >= k) {
+                    innecesarios = i;
+                    break;
+                }
+            }
+            cout<<k+innecesarios<<"\n";
+        }
+    }
+
+    return 0;
+}
