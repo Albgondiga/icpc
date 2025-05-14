8998f0ad0d6bd7d7a1d299c2cbb110529be46252
Alberto Gonzalez Avendano <albertoga03@gmail.com>
2025-05-14 15:45:47 -0600

    Codigo inicial de K de GPMX 1 2025

--- GPMX/2025/1/k/k.cpp
+++ GPMX/2025/1/k/k.cpp
@@ -15,134 +15,142 @@ using namespace __gnu_pbds;
 #define debug 1
 #define ifd if (debug)
 
-vector<bool> visited; // keeps track of which vertices are already visited
-vector<vector<int>> adj;
-vector<vector<int>> components;
-vector<vector<int>> adj_cond;
-vector<vector<int>> adj_cond_rev;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
 
-vector<int> costo;
-vector<int> ganancia;
-vector<int> costoCond;
-vector<int> inGrado;
-vector<int> inGradoCond;
+// Kosaraju
+// necesita el grafo en g y el grafo reverso en gr. En comp te dice para cada vertice a 
+// que componente fuertemente conexa pertenece. En gcc te deja el DAG de componentes 
+// fuertemente conexas (este puede tener aristas repetidas, si hay mas de una que va de la componente x a la y)
+int n,m; vvi g,gr,gcc;
+vb visitado; vi comp, orden, costo, costo_gcc;
+void dfs1(int v){
+    if(visitado[v]) return;
+    visitado[v] = true;
+    for(auto u : gr[v]) dfs1(u);
+    orden.push_back(v); }
+void dfs2(int v, int c){
+    comp[v] = c;
+    costo_gcc[c] += costo[v];
+    for(auto u : g[v]) {
+        if(comp[u] == -1) dfs2(u,c);
+        else if(comp[u] != c) gcc[c].push_back(comp[u]);}}
+void scc(){
+    visitado = vb(n,false); for (int i = 0; i < n; i++) dfs1(i);
+    int c = 0;
+    for (int i = 0; i < n; i++) {
+        if(comp[orden[i]] != -1) continue;
+        gcc.push_back({});
+        costo_gcc.push_back(0);
+        dfs2(orden[i],c); c++;} }
 
-// runs depth first search starting at vertex v.
-// each visited vertex is appended to the output vector when dfs leaves it.
-void dfs(int v, vector<vector<int>> const& adj, vector<int> &output) {
-    visited[v] = true;
-    for (auto u : adj[v])
-        if (!visited[u])
-            dfs(u, adj, output);
-    output.push_back(v);
-}
-
-// input: adj -- adjacency list of G
-// output: components -- the strongy connected components in G
-// output: adj_cond -- adjacency list of G^SCC (by root vertices)
-void strongly_connected_components() {
-    int n = adj.size();
-    components.clear(), adj_cond.clear();
-
-    vector<int> order; // will be a sorted list of G's vertices by exit time
-
-    visited.assign(n, false);
-
-    // first series of depth first searches
-    for (int i = 1; i < n; i++)
-        if (!visited[i])
-            dfs(i, adj, order);
-
-    // create adjacency list of G^T
-    vector<vector<int>> adj_rev(n);
-    for (int v = 1; v < n; v++)
-        for (int u : adj[v])
-            adj_rev[u].push_back(v);
-
-    visited.assign(n, false);
-    reverse(order.begin(), order.end());
-
-    vector<int> roots(n, 0); // gives the root vertex of a vertex's SCC
-
-    int root = 1;
-    costoCond.push_back(0);
-    // second series of depth first searches
-    for (auto v : order)
-        if (!visited[v]) {
-            std::vector<int> component;
-            dfs(v, adj_rev, component);
-            components.push_back(component);
-            //int root = *min_element(begin(component), end(component));
-            costoCond.push_back(0);
-            for (auto u : component) {
-                roots[u] = root;
-                costoCond[root] += costo[u];
+// Dinic
+const ll INF = 1e18;
+struct Dinic {
+    int src,dst;
+    vector<int>dist,q,work;
+    struct edge{int to,rev;ll f,cap;};
+    vector<vector<edge>> G;
+    Dinic (int x):G(x),dist(x),q(x),work(x){}
+    void add_edge(int s, int t, ll cap){
+        G[s].push_back({t,(int)G[t].size(),0,cap});
+        G[t].push_back({s,(int)G[s].size()-1,0,0});
+    }
+    bool dinic_bfs(){
+        fill(dist.begin(), dist.end(),-1);dist[src]=0;
+        int qt=0;q[qt++]=src;
+        for(int qh=0;qh<qt;qh++){
+            int u=q[qh];
+            for(int i = 0; i < (int)G[u].size(); i++){
+                edge &e=G[u][i];int v=G[u][i].to;
+                if(dist[v]<0&&e.f<e.cap)dist[v]=dist[u]+1,q[qt++]=v;
             }
-            root++;
         }
-
-    // add edges to condensation graph
-    adj_cond.assign(root, {});
-    for (int v = 1; v < n; v++)
-        for (auto u : adj[v])
-            if (roots[v] != roots[u])
-                adj_cond[roots[v]].push_back(roots[u]);
-}
-
-void dfsCostoAcumulado(int v, int suma) {
-    if (visited[v]) return;
-    visited[v] = true;
-    ganancia[v] = suma + costo[v];
-    for (int u : adj_cond[v]) {
-        dfsCostoAcumulado(u, ganancia[v]);
+        return dist[dst]>=0;
     }
-}
-
-int dfsMaximos(int v) {
-    if (visited[v]) return;
-    visited[v] = true;
-    int maximo = 0;
-    for (int u : adj_cond[v]) {
-        maximo = max(maximo, dfsMaximos(u));
+    ll dinic_dfs(int u, ll f){
+        if(u==dst)return f;
+        for(int &i=work[u];i<(int)G[u].size();i++){
+            edge &e=G[u][i];
+            if(e.cap<=e.f)continue;
+            int v=e.to;
+            if(dist[v]==dist[u]+1){
+                ll df=dinic_dfs(v,min(f,e.cap-e.f));
+                if(df>0){e.f+=df;G[v][e.rev].f-=df;return df;}
+            }
+        }
+        return 0;
     }
-    return maximo;
-}
+    ll max_flow(int _src, int _dst){
+        src=_src,dst=_dst;
+        ll result=0;
+        while(dinic_bfs()){
+            fill(work.begin(), work.end(),0);
+            while(ll delta=dinic_dfs(src,INF))result+=delta;
+        }
+        return result;
+    }
+    // Especifico del problema
+    int max_closure() {
+        vector<bool> visited(G.size(), false);
+        queue<int> q;
+        q.push(src);
+        visited[src] = true;
+        int ans = 0;
+        while (!q.empty()) {
+            int u = q.front(); q.pop();
+            for (const edge& e : G[u]) {
+                if (!visited[e.to] and e.f < e.cap) {
+                    visited[e.to] = true;
+                    q.push(e.to);
+                    ans += costo_gcc[e.to];
+                }
+            }
+        }
+        return ans;
+    }
+    
+};
 
 int main() {
     cin.tie(0);
     ios_base::sync_with_stdio(false);
 
-    int n, m; cin>>n>>m;
-    adj.resize(n+1), costo.resize(n+1), ganancia.resize(n+1);
-    inGrado.assign(n+1,0);
+    cin>>n>>m;
+    visitado.assign(n,false), 
+    comp.resize(n),
+    costo.resize(n), 
+    orden.assign(n,0),
+    g.resize(n),
+    gr.resize(n);
 
-    for (int i = 1; i <= n ; i++) {
+    for (int i = 0; i < n; i++) {
         cin>>costo[i];
     }
     for (int i = 0; i < m; i++) {
-        int a, b; cin>>a>>b;
-        adj[a].push_back(b);
-        inGrado[b]++;
+        int u, v; cin>>u>>v;
+        u--, v--;
+        orden[v]++;
+        g[u].push_back(v);
+        gr[v].push_back(u);
     }
 
-    // Encuentro el costo acumulado de cada nodo
-    for (int v = 1; v <= n; v++) {
-        if (inGrado[v] == 0) 
+    scc();
+
+    Dinic dinic(gcc.size()+2);
+    for (int u = 0; u < gcc.size(); u++) {
+        for (int& v : gcc[u]) {
+            dinic.add_edge(u,v,INF);
+        }
+    }
+    // src es n, dst es n+1
+    for (int u = 0; u < gcc.size(); u++) {
+        if (costo[u] >= 0) dinic.add_edge(n, u, costo[u]);
+        else dinic.add_edge(u,n+1,-costo[u]);
     }
 
-    // Encuentro las componentes
-    strongly_connected_components();
-    // Guardo el grafo de componentes invertido
-    n = adj_cond.size();
-    for (int v = 1; v < n; v++)
-        for (int u : adj_cond[v])
-            adj_cond_rev[u].push_back(v);
-
-    vector<int> costo(n);
-    vector<int> ganancia(n);
-    vector<int> costoCond(n);
-    for ()
-
+    cout<<dinic.max_closure()<<endl;
 
     return 0;
 }
