a950bc69d616a2bb5bdeaa8d65bbcf3f6ecf7640
Alberto González Avendaño <albertoga03@gmail.com>
2025-05-20 12:46:24 -0600

    Codeforce

--- Codeforces/Contests/986/b/b.cpp
+++ Codeforces/Contests/986/b/b.cpp
@@ -7,8 +7,7 @@ typedef pair<int, int> ii;
 typedef pair<ll,ll> pll;
 
 #include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp> 
-using namespace __gnu_pbds; 
+#include <ext/pb_ds/tree_policy.hpp>  
   
 #define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
 
