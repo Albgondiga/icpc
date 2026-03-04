1c23403a432424fb205ae3687ce93c29524e2545
Alberto González Avendaño <albertoga03@gmail.com>
2026-03-04 11:50:13 -0600

    LCS

---
+++ AtCoder/DP/f/f.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std; 
+#define forr(i, a, b) for (int i=a; i<b; i++)
+#define forn(i, n) forr(i, 0, n)
+#define sz(v) (int(v.size()))
+#define all(v) begin(v), end(v)
+#define pb push_back
+#define pp pop_back
+#define fst first
+#define snd second
+#define fore(e,c) for(const auto &e : (c))
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<ll,ll> pll;
+
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+  
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+
+#define debug 1
+#define ifd if (debug)
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    string s, t; cin>>s>>t;
+
+    int n = s.size();
+    int m = t.size();
+
+    // dp[i][j] tiene LCS entre s[0...i-1] y t[0...j-1]
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (s[i-1] == t[j-1])
+                dp[i][j] = dp[i-1][j-1] + 1;
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+
+    int i = n, j = m;
+    string ans = "";
+    while (i > 0 and j > 0) {
+        if (s[i-1] == t[j-1]) {
+            ans.push_back(s[i-1]);
+            i--; j--;
+        } else if (dp[i-1][j] >= dp[i][j-1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+    reverse(ans.begin(), ans.end());
+    cout<<ans<<"\n";
+
+    return 0;
+}
